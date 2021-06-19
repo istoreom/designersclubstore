727765994fb7a219b3d3bfa64b311fb17b280b51
msscoman <64360317+msscoman@users.noreply.github.com>
2021-06-19 15:55:50 +0400

    Delete wk_coupons directory

--- wk_coupons/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#################################################################################
-#
-#    Copyright (c) 2016-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
-#
-#################################################################################
-from . import wk_coupons
-from . import coupon_history
-from . import  res_config
