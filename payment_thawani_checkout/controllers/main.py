4e2ebfdd4061a869b87e9940188bca830e1f25fa
iStore.om <97754328+istoreom@users.noreply.github.com>
2022-12-10 00:34:41 +0400

    [ADD] app payment_thawani_checkout

---
+++ payment_thawani_checkout/controllers/main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# License URL : https://store.webkul.com/license.html/
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+#################################################################################
+import logging
+import werkzeug
+import json
+import pprint
+import odoo
+
+from odoo import http, _
+from odoo.http import request, Response
+from odoo.addons.payment.controllers.post_processing import PaymentPostProcessing
+
+_logger = logging.getLogger(__name__)
+
+class ThawaniCheckout(http.Controller):
+
+    @http.route('/payment/thawani_checkout/success', type='http', auth="public", csrf=False, website=True)
+    def thawani_checkout_success(self, reference, **post):
+        params = {
+            "reference" : reference,
+            "state" : "pending"
+        }
+        request.env['payment.transaction'].sudo()._handle_feedback_data('thawani_checkout', params)
+        return werkzeug.utils.redirect('/payment/status')
+
+    @http.route('/payment/thawani_checkout/cancel', type='http', auth="public", website=True)
+    def thawani_checkout_cancel(self, reference, **post):
+        params = {
+            "reference" : reference,
+            "state" : "cancel"
+        }
+        request.env['payment.transaction'].sudo()._handle_feedback_data('thawani_checkout', params)
+        return werkzeug.utils.redirect('/payment/status')
+
+    @http.route('/thawani/checkout/webhook/interface', type='json', auth='public')
+    def thawani_checkout_webhook_interface(self, **post):
+        payload = {}
+        if request.httprequest.data:
+            payload = json.loads(request.httprequest.data)
+            _logger.info("Thawani Checkout Webhook Post Data:\n%s", pprint.pformat(payload))
+            data = payload.get('data')
+            if data and data.get('client_reference_id'):
+                params = {
+                    "amount" : data.get('total_amount'),
+                    "currency" : data.get('currency'),
+                    "reference" : data.get('client_reference_id'),
+                    "state" : data.get('payment_status'),
+                    "acquirer_reference" : data.get('invoice'),
+                    "session_id" : data.get('session_id'),
+                }
+                request.env['payment.transaction'].sudo()._handle_feedback_data('thawani_checkout',params)
+        return Response('success', status=200)
